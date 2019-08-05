ebc586ac52cb0b755c6adfde1b6f03176de932fd
Erin Volcan <erinvolcan@gmail.com>
2019-08-05 19:08:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ new_numbers = []
 def square_array(numbers)
   index = 0 
   while index < numbers.length do
-    new_array << numbers[index]**2 
+    new_numbers << numbers[index]**2 
     index += 1 
  end
 end
